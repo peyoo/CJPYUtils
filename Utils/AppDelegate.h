3c9306925047ec415c03fd0c559e0f915af2e948
peyoo <peyoo.zh@gmail.com>
2013-03-26 13:54:45 +0800

    Initial Commit

---
+++ Utils/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Utils
+//
+//  Created by 彭 勇 on 13-3-26.
+//  Copyright (c) 2013年 cjpystuido. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
